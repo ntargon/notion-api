290547561b3caa92b022c85daf665ca47448d6cf
ntargon <argon19951128@gmail.com>
2024-09-14 11:20:23 +0900

    feat: 定期タスクの登録機能の追加

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import click
 import os
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
-
+from croniter import croniter
 
 # 環境変数を`.env`から読み込む
 load_dotenv()
@@ -12,6 +12,7 @@ load_dotenv()
 
 NOTION_API_KEY = os.getenv('NOTION_API_KEY', '')
 DATABASE_ID = os.getenv('DATABASE_ID', '')
+DATABASE_ID_RECURRING_TASK = os.getenv('DATABASE_ID_RECURRING_TASK', '')
 LINE_NOTIFY_API_URL = "https://notify-api.line.me/api/notify"
 LINE_NOTIFY_TOKEN = os.getenv('LINE_NOTIFY_TOKEN', '')
 
@@ -60,34 +61,89 @@ def get_upcoming_tasks(database_id):
         for result in results]
     return tasks
 
-# def create_task(title: str, due_date: datetime):
-#     """
-#     notionにタスクを追加する
-#     """
+def get_recurring_tasks(database_id):
+    """Notionから定期タスク一覧を取得する"""
 
-#     json_data = {
-#         'parent': { 'database_id': DATABASE_ID },
-#         'properties': {
-#             'タスク名': {
-#                 'title': [
-#                     {
-#                         'text': {
-#                             'content': title
-#                         }
-#                     }
-#                 ]
-#             },
-#             '期限': {
-#                 "date": {
-#                     "start": due_date.strftime('%Y-%m-%d')
-#                 }
-#             }
-#         },
-#     }
 
-#     # データ作成
-#     response = requests.post(URL_PAGES, headers=NOTION_API_HEADER, json=json_data)
-#     print(response)
+    response = requests.post(URL_DATABASES.format(database_id=database_id), headers=NOTION_API_HEADER)
+    response.raise_for_status()
+    results = response.json().get('results', [])
+
+    tasks = [
+        {
+            'タスク名': result['properties']['タスク名']['title'][0]['text']['content'],
+            'cron': result['properties']['cron']['rich_text'][0]['plain_text'],
+            'id': result['id'],
+        }
+    for result in results]
+
+
+    return tasks
+
+def get_registered_recurring_tasks(database_id: str, recurring_task_id: str):
+    """1ヶ月先のタスクのうち、定期タスクの一覧を取得する"""
+
+    tz_jst = timezone(timedelta(hours=9))
+    dt_now = datetime.now(tz_jst)
+    four_weeks_later = dt_now + timedelta(weeks=4)
+    query = {
+        "filter": {
+            "and": [
+                {"property": "期限", "date": {"on_or_before": four_weeks_later.isoformat()}},
+                {"property": "定期タスク名", "relation": {"contains": recurring_task_id}},
+            ]
+        }
+    }
+
+    response = requests.post(URL_DATABASES.format(database_id=database_id), headers=NOTION_API_HEADER, json=query)
+    response.raise_for_status()
+    results = response.json().get('results', [])
+
+    tasks = [
+        {
+            'タスク名': result['properties']['タスク名']['title'][0]['text']['content'],
+            '期限': result['properties']['期限']['date']['start'],
+            '定期タスクid': result['properties']['定期タスク名']['relation'][0]['id'],
+        }
+    for result in results]
+
+    return tasks
+
+def create_task(database_id: str, title: str, due_date: datetime, recurring_task_id: str):
+    """
+    notionにタスクを追加する
+    """
+
+    json_data = {
+        'parent': { 'database_id': database_id },
+        'properties': {
+            'タスク名': {
+                'title': [
+                    {
+                        'text': {
+                            'content': title
+                        }
+                    }
+                ]
+            },
+            '期限': {
+                "date": {
+                    "start": due_date.strftime('%Y-%m-%d')
+                }
+            },
+            '定期タスク名': {
+                "relation": [{
+                    "id": recurring_task_id
+                }]
+            },
+        },
+    }
+
+    # データ作成
+    response = requests.post(URL_PAGES, headers=NOTION_API_HEADER, json=json_data)
+    response.raise_for_status()
+
+    click.echo(f'{title} with due date = {due_date} has been created.')
 
 
 @click.group()
@@ -114,6 +170,39 @@ def notify_upcoming_tasks():
     """
     tasks = get_upcoming_tasks(DATABASE_ID)
     send_line_notify(compose_message_for_upcoming_tasks(tasks))
+
+def register_recurring_task(recurring_task):
+
+    # 4週間先までのタスクを取得
+    tasks = get_registered_recurring_tasks(DATABASE_ID, recurring_task['id'])
+    # tasks = []
+
+    tz_jst = timezone(timedelta(hours=9))
+    dt_now = datetime.now(tz_jst)
+    four_weeks_later = dt_now + timedelta(weeks=4)
+
+    iter = croniter(recurring_task['cron'], dt_now)
+    for _i in range(4*7): # 最大28個登録する
+        dt: datetime = iter.get_next(datetime)
+
+        if dt > four_weeks_later:
+            break
+
+        if dt.strftime('%Y-%m-%d') in [task.get('期限') for task in tasks]:
+            # すでに登録されている
+            continue
+
+        create_task(DATABASE_ID,
+                    f"[定期] {recurring_task['タスク名']}",
+                    dt,
+                    recurring_task['id'])
+
+@cli.command()
+def register_recurring_tasks():
+    """1ヶ月先までの定期タスクを登録する"""
+    tasks = get_recurring_tasks(DATABASE_ID_RECURRING_TASK)
+    for task in tasks:
+        register_recurring_task(task)
 
 def main():
     cli()
